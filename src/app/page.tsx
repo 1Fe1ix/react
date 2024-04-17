3a5860690ae9cff410451ccba1f9a9976e79a468
Felix Hoffmann <hoffmannfelix.3@gmail.com>
2024-04-17 23:52:28 +0200

    Softwaretech Project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,67 @@
-import Image from "next/image";
-import styles from "./page.module.css";
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+ return(
+  <div class="mainFrame">
+  
+  <div class="logo"> <img src="stc-logo.svg" alt="STC Logo" width="100" height="50"></img> </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+  <div>
+  <table >
+  <tr>
+    <th>Name</th>
+    <th>Messages</th>
+    <th>Button possibility</th>
+  </tr>
+  <tr>
+    <td>Eric Freeman</td><td>Ask's for Vacation</td><td><button class="accepButton">accept</button><button class="reject">reject</button></td>
+  </tr>
+  <tr>
+    <td>Eric Freeman</td><td>Reducing overtime</td><td><button class="accepButton">accept</button><button class="reject">reject</button></td>  
+  </tr>
+  <tr>
+    <td>Jessica Grinder</td><td>Ask's for Vacation</td><td><button class="accepButton">accept</button><button class="reject">reject</button></td>  
+  </tr>
+  <tr>
+    <td>Anna Greenfield</td><td>HR confirmed Anna as sick</td><td><button class="accepButton">accept</button><button class="reject">reject</button></td>  
+  </tr>
+  <tr>
+    <td>Hanna Buckford</td><td>Takes too little break at </td><td><button class="accepButton">accept</button><button class="reject">reject</button></td>  
+  </tr>
+  <tr>
+    <td>Samuel Jackson</td><td>Exceeded maximum ammount of overhours</td><td><button class="accepButton">accept</button><button class="reject">reject</button></td>  
+  </tr>
+  <tr>
+    <td>Samuel Jackson</td><td>Exceeded maximum ammount of overhours</td><td><button class="accepButton">accept</button><button class="reject">reject</button></td>  
+  </tr>
+  <tr>
+    <td>Samuel Jackson</td><td>Exceeded maximum ammount of overhours</td><td><button class="accepButton">accept</button><button class="reject">reject</button></td>  
+  </tr>
+  <tr>
+    <td>Samuel Jackson</td><td>Exceeded maximum ammount of overhours</td><td><button class="accepButton">accept</button><button class="reject">reject</button></td>  
+  </tr>
+  </table> 
+  </div>
+  </div>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+);
 }
